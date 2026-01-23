c0389726f497ddea41f06ae27a395e187f4f9039
Andy <andreas@heynanny.com>
2026-01-23 07:34:37 +0100

    fix: import type for interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import Dropdown from './components/Dropdown';
 import MultiSelect from './components/MultiSelect';
 import SelectCountry from './components/SelectCountry';
-import { IDropdownRef } from './components/Dropdown/model';
-import { IMultiSelectRef } from './components/MultiSelect/model';
-import { ISelectCountryRef } from './components/SelectCountry/model';
+import type { IDropdownRef } from './components/Dropdown/model';
+import type { IMultiSelectRef } from './components/MultiSelect/model';
+import type { ISelectCountryRef } from './components/SelectCountry/model';
 
 export {
   Dropdown,
